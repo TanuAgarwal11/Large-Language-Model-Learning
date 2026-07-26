cc1b75c5f34612ea489a98aae275e92c17be9a13
Tanu Agarwal <agarwaltanu564@gmail.com>
2026-07-26 21:28:58 +0530

    convert tokens to token IDs

--- tokenization.py
+++ tokenization.py
@@ -12,3 +12,11 @@ tokens = tokenizer.tokenize(text)
 print("Original text:", text)
 print("Tokens:", tokens)
 print("Number of tokens:", len(tokens))
+
+#convert tokens to token ids ---- (it return 5 ids start of sequence , token ids , end of sequence)
+# #Easy way to remember:
+# CLS → Classification → Start of input
+# SEP → Separator → Separates/ends sentences
+
+token_ids = tokenizer.encode(text)
+print("Token IDs:", token_ids) 
